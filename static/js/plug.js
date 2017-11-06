4af7b30b9b010139dc2fcedc224b3c43d0f4212a
“forheyin <forheyin@163.com>
2017-11-06 20:07:33 +0800

    confirm

--- static/js/plug.js
+++ static/js/plug.js
@@ -1,4 +1,4 @@
-//toast组件
+//  toast组件
 Vue.component('v-toast', {
     template: `<transition name="out">
             <div class="toast" v-show="flag">
@@ -35,15 +35,15 @@ Vue.component('v-toast', {
             // setTimeout(function() {
             //     that.flag = false;
             // },800)
-        }
+        },
     }
 });
-function toaFun(item) {
-    console.log(typeof item)
-    if (typeof item === 'string' || typeof item === 'number' ) {
+function Toast(item) {
+    // console.log(typeof item)
+    if (typeof item === 'string' || typeof item === 'number') {
         vm.toastText = item;
         console.log(1)
-    } else if (typeof item == 'undefined' || item ===null) {
+    } else if (typeof item == 'undefined' || item === null) {
         vm.toastText = '这不是演习';
         console.log(2)
     } else {
@@ -52,4 +52,56 @@ function toaFun(item) {
         vm.toastText = item.text;
     }
     vm.$refs.sonMet.fade();
-}
+};
+
+//  confirm 组件
+Vue.component('v-confirm', {
+    template: `<transition name="out">
+        <div class="confirm" v-show="flag" @tap="flag = false">
+            <div class="m_con">
+                <div class="m_head">{{conTitle}}</div>
+                <div class="m_body">
+                    <slot name="conText"> 
+                        
+                    </slot>        
+                </div>
+                <div class="m_foot" v-if="contype==='1'">
+                    <span @tap="flag=false">取消</span>
+                    <span @tap="conFun">确定</span>
+                </div>
+                <div class="m_foot" v-if="contype==='2'">
+                    <span @tap="conFun">确定</span>
+                </div>
+            </div>
+        </div>
+    </transition>`,
+    data: function () {
+        return {
+            flag: false
+        }
+    },
+    props: {
+        conTitle: {
+            type: String,
+            default: '操作提示'
+        },
+        contype: {
+            type: String,
+            default: '1'
+        },
+    },
+    created: function () {
+    },
+    methods: {
+        conFade: function () {
+            this.flag = true;
+        },
+        conFun: function () {
+            if (this.contype === '1') {
+                this.$emit('con','转过来');
+            }
+            this.flag = false;
+            console.log(this.contype)
+        }
+    }
+});
